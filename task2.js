6473b0482f2c42b743883f028c275c34c1b4e0e3
Dhairya Veera <dhairyaveera79@gmail.com>
2020-08-10 20:23:24 +0530

    completed and added task 2

---
+++ task2.js
@@ -0,0 +1,27 @@
+const { joke } = require("express");
+const axios = require('axios');
+
+function jokes(){
+    axios.get('https://sv443.net/jokeapi/v2/joke/Any').then(joke => {
+        var JOKE;
+        if (joke.data.type == "single"){
+            JOKE = joke.data.joke;
+        }
+        else{
+            JOKE = {
+                "setup": joke.data.setup,
+                "delivery": joke.data.delivery
+            }
+        }
+        var joke_obj = {
+            "category": joke.data.category,
+            "type": joke.data.type,
+            "joke": JOKE,
+            "id": joke.data.id,
+            "language": joke.data.lang
+        };
+        console.log(joke_obj);
+    });
+}
+
+jokes();
